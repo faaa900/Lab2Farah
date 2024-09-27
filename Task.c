7f006985120f3cf5bc8db1b7ccdcc5b0d80e14ab
faaa900 <faaa900@uowmail.edu.au>
2024-09-27 12:24:33 +0400

    Add files via upload

---
+++ Task.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void)
+int calculate_change(coins_total,price)
+{
+int menu_number;
+float coins_total,coins_E=3.5,coins_C=4.5,coins_M=5.5;
+printf("enter a number from the list: \n 1.Espresso (%f) \n 2.Cappuccino (%f) \n 3.Mocha (%f)",coins_E,coins_C,coins_M);
+scanf("%i",&menu_number);
+
+printf("Insert coins.Use coins of 1 and 0.5 only");
+scanf("%f",&coins_total);
+}
